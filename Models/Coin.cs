74af8b785a8f2bc25dd9156a63086db89d817462
Larissa11270 <lariamaral5566@gmail.com>
2025-09-06 11:52:28 -0300

    coisa nova bem fofis :)

---
+++ Models/Coin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTeste.Models
+{
+    public class Coin
+    {
+        public string Lado { get; set; } = string.Empty;
+
+        // método - geralmente usa verbos
+        public string Jogar()
+        {
+            // next - sorteia um numero entre 0 e o numero dentro do parametro
+            // Random é um classe 
+            int ladoSorteado = new Random().Next(2);
+            //operador ternário
+            // Ponto de interrogação é um if escondidinho 
+            Lado = ladoSorteado == 0 ? "Cara" : "Coroa";
+            return Lado;
+        }
+
+        //polimorfismo bem fofo
+        public string Jogar(string ladoEscolhido)
+        {
+            int ladoSorteado = new Random().Next(2);
+            Lado = ladoSorteado == 0 ? "Cara" : "Coroa";
+
+            string resultado = (Lado == ladoEscolhido) ?
+                $"Parabéns, você pediu {ladoEscolhido}  e deu {Lado}" :
+                $"Que pena, você pediu {ladoEscolhido} e deu {Lado}";
+            // ❤
+            return resultado;
+        }
+    }
+}
---
+++ ViewModels/CoinViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppTeste.Models; 
+using CommunityToolkit.Mvvm.ComponentModel; //import
+
+namespace AppTeste.ViewModels
+{
+    // dois pontos (:) seria o extendes (herança)
+    // coloca underline para referenciar que é uma variavel global,...
+    // ... percente a classe como um todo
+    public partial class CoinViewModel : ObservableObject
+    {
+        public string _ladoEscolhido = string.Empty;
+        public string _imagem = string.Empty;
+        public string _resultado = string.Empty;
+
+        public void Flip()
+        {
+            Coin coin = new Coin();
+            _resultado = coin.Jogar(_ladoEscolhido);
+        }
+    }
+}
